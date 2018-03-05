bf504755aaf2f7721ee5bd601b989b3dcf67a9d7
Amit Athani <donathani@gmail.com>
2018-03-04 23:31:56 -0700

    using busboy

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(function(req, res, next) {
   next();
 });
 app.use(logger('dev'));
-// busboy.extend(app)
+busboy.extend(app)
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
