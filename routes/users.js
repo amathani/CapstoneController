13fefaab7858759030bb90968f310f51885de83f
Amit Athani <donathani@gmail.com>
2018-03-04 23:51:05 -0700

    e

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,6 @@
 var express      = require('express');
 var router       = express.Router();
 
-var upload = multer({ dest: '/tmp/' });
-var router = express.Router();
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
